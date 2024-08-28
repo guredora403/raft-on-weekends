cc48e1ea0dd86962c21f9df296f40a3cceefe6ad
moz <moz@sfc.wide.ad.jp>
2024-08-28 10:54:16 +0900

    add: 発表用

--- raft/state.py
+++ raft/state.py
@@ -8,13 +8,12 @@ class State:
     def __init__(self, node):
         self.node = node
         self.loop = self.node.loop or asyncio.get_event_loop()
-        
-        # self.counter = {}
+        self.counter = {}
 
     def request_handler(self, data):
         received_from = convert_ipv4_to_hostname(data["sender"][0])
         
-        # # サンプルプログラム、ノード毎にカウントを増やす
+        # サンプルプログラム、ノード毎にカウントを増やす
         self.counter[received_from] = max(self.counter.get(received_from, 0), int(data["data"])) + 1
         logger.info("Counter[{}] = {}".format(received_from, self.counter[received_from]))
 
